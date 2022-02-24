a55316cbe3cccb99aa407a8a727770a6713aac08
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2022-02-24 20:29:15 +0000

    delete the nonfunctional serve-mkdocs script

--- .devcontainer/serve-mkdocs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Pseudo-daemonized `mkdocs serve` for running in the background on devcontainers
-
-nohup mkdocs serve > /workspaces/c4g-spr22-blis.github.io/.devcontainer/mkdocs.log &
